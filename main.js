4e6844a72b1af3ee82c858ffd6ef82f0ddb81445
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-08-22 00:33:52 +0300

    $ git commit -m

--- main.js
+++ main.js
@@ -6,17 +6,22 @@ input.style = `
 `
 
 const ourGallery = []
-input.onchange = function (event) {
-  const reader = new FileReader()
-  reader.readAsDataURL(event.target.files[0])
-  reader.onload = function (event) {
-    let image = document.body.appendChild(document.createElement('img'))
-    image.src = event.target.result
-    image.style = `
-      width: 150px;
-      display: inline-block;
-      margin: 0 10px;
-    `
-    ourGallery.push(image)
+input.onchange = (function () {
+  const bufferArray = []
+  return function (event) {
+    bufferArray.push(event.target.files[0])
+    const reader = new FileReader()
+      reader.readAsDataURL(bufferArray[0])
+      reader.onload = function (event) {
+        let image = document.body.appendChild(document.createElement('img'))
+        image.src = event.target.result
+        image.style = `
+          width: 150px;
+          display: inline-block;
+          margin: 0 10px;
+        `
+        bufferArray.shift()
+        ourGallery.push(image)
+      }
   }
-}
+})()
