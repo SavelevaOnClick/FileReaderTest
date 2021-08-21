98c653f688ae60a0154577dc7998d81164c86327
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-08-22 01:42:55 +0300

    second version for testing

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ const input = document.body.appendChild(document.createElement('input'))
 input.type = 'file'
 input.setAttribute('multiple', '')
 input.style = `
-   // width: 300px;
+    width: 300px;
 `
+function parse (array) {
+      const reader = new FileReader()
+  reader.readAsDataURL(array[0])
+ reader.onload = function (event) {
+  let image = document.body.appendChild(document.createElement('img'))
+  image.src = event.target.result
+  image.style = `
+   width: 150px;
+    display: inline-block;
+    margin: 0 10px;
+ `
+  array.shift()
+  ourGallery.push(image)
+  reader.readyState === 2 && array.length !==0 && parse(array)
+ }
+}
 
 const ourGallery = []
-input.onchange = (function () {
-  const bufferArray = []
-  return function (event) {
-    bufferArray.push(event.target.files[0])
-    const reader = new FileReader()
-      reader.readAsDataURL(bufferArray[0])
-      reader.onload = function (event) {
-        let image = document.body.appendChild(document.createElement('img'))
-        image.src = event.target.result
-        image.style = `
-          width: 150px;
-          display: inline-block;
-          margin: 0 10px;
-        `
-        bufferArray.shift()
-        ourGallery.push(image)
-      }
+input.onchange = function (event) {
+      const bufferArray = Array.from(event.target.files)
+      parse(bufferArray)
   }
-})()
